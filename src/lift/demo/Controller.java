cbaace316163d828ff5b8f926803d75f0bef1933
kees95 <kuijl95@gmail.com>
2016-10-06 22:08:27 +0200

    comments door Kees
    Er zijn comments toegevoegd aan de code

--- src/lift/demo/Controller.java
+++ src/lift/demo/Controller.java
@@ -94,6 +94,7 @@ public class Controller
 		
 		int raw_aanvraag = kooi.getSwitchValues();
 		
+		
 		if(raw_aanvraag == 1){
 			liftvraag0 = true;
 		}
@@ -175,113 +176,115 @@ public class Controller
 
 	//Functie om de Motor aan te sturen
 	public static void motorAansturing(){
-		if(huidigeLocatie == 1){
-			if (liftvraag0 == true){
-				liftvraag0 = false;
+		if(huidigeLocatie == 1){//als de lift op de beganegrond is word het volgende uitgevoerd:
+			if (liftvraag0 == true){//als de liftknp van verdieping ingedrukt is voer het volgende uit:
+				liftvraag0 = false;// Het geheugen van de liftknop word uitgezet
 				motorspeed = 0;
 				lift.setMotorSpeed(motorspeed);
+				//zet de liftdeuren open wacht 5 seconde en sluit de liftdeuren daarna weer
 				lift.setDoorOpen();
 				sleep(5000);
 				lift.setDoorClose();
 				sleep(5000);				
-				motorRichting = true;
+				motorRichting = true; //zet de richting van de motor weer naar boven
 			}
-			else if (liftvraag1 == true || liftvraag2 == true){
+			else if (liftvraag1 == true || liftvraag2 == true){//als de liftknoppen op 1 en 2 zijn inggedrukt ga met 2 snelheid naar boven
 				motorspeed = 2;
 			}
 		}
-		if (huidigeLocatie == 2){
+		if (huidigeLocatie == 2){//motor snelheid blijf 2
 			motorspeed = 2;
 		}
-		if (huidigeLocatie == 3){
+		if (huidigeLocatie == 3){//versnel de lift met 2 snelheid naar 4
 			motorspeed = 4;
 		}
-		if (huidigeLocatie == 4){
+		if (huidigeLocatie == 4){// als de lift niet naar de 1e verdieping moet versnel met 2 naar 6
 			if((liftvraag2 == true && liftvraag1 == false) || (liftvraag0 == true && motorspeed == 8)){
 				motorspeed = 6;
 			}
-			else if (liftvraag0 == false && motorspeed != 6){
+			else if (liftvraag0 == false && motorspeed != 6){//als de lift wel naar de eerste verdieping moet verlaag de snelheid met 2 naar 4
 				motorspeed = 2;
 			}
 		}
-		if (huidigeLocatie == 5){
-			if (motorRichting == true){
-				if (liftvraag1 == true && motorspeed != 6){
-					motorspeed = 0;
+		if (huidigeLocatie == 5){// als de lift op de 1e verdieping is voer het volgende uit
+			if (motorRichting == true){//als de motor naar boven gaat voer het volgende uit
+				if (liftvraag1 == true && motorspeed != 6){//als de lift naar de 1e verdieping moet en niet 6 snelheid heeft voer het volgende uit
+					motorspeed = 0;//zet de motor uit
 					lift.setMotorSpeed(motorspeed);
-					lift.setDoorOpen();
+					lift.setDoorOpen();//zet de deur open en zet hem na 5 seconden weer dicht
 					sleep(5000);
 					lift.setDoorClose();
 					sleep(5000);
-					liftvraag1 = false;
+					liftvraag1 = false;//de liftvraag word weer uit gezet
 				}
-				if(liftvraag2 == true && motorspeed == 6){
+				if(liftvraag2 == true && motorspeed == 6){//als de lift naar verdieping 2 gaat met 6 snelheid verhoog de snelheid naar 8
 					motorspeed = 8;
 				}
-				if (liftvraag2 == true && motorspeed == 0){
+				if (liftvraag2 == true && motorspeed == 0){//als de lift naar verdieping 2 moet maar nog stil staat, start de motor met 2 snelheid
 					motorspeed = 2;
 				}
-				if (motorspeed == 0 && liftvraag2 == false){
+				if (motorspeed == 0 && liftvraag2 == false){//als de motor uit staat en de lift niet naar verdieping 2 moet zet de motor richting naar beneden
 					motorRichting = false;
 				}
 			}
-			if (motorRichting == false){
-				if (liftvraag1 == true && motorspeed == 6){
+			if (motorRichting == false){// als de motorrichting naar beneden staat voer het volgende uit
+				if (liftvraag1 == true && motorspeed == 6){// als de motor met 6 snelheid gaat verhoog deze met 2
 					motorspeed = 8;
 				}
-				if (liftvraag1 == true && motorspeed == 2){
-					motorspeed = 0;
+				if (liftvraag1 == true && motorspeed == 2){//als de lift naar verdieping 1 moet en de snelheid is 2 voer het volgende uit
+					motorspeed = 0;//zet de motor uit
 					lift.setMotorSpeed(motorspeed);
-					lift.setDoorOpen();
+					lift.setDoorOpen();//open de liftdeuren voor 5 seconden en sluit deze weer na 5 seconden
 					sleep(5000);
 					lift.setDoorClose();
 					sleep(5000);
-					liftvraag1 = false;
+					liftvraag1 = false;//de liftaanvraag is afgehandeld
 				}
-				if (liftvraag0 == true){
+				if (liftvraag0 == true){//als de lift naar verdieping 0 moet verhoog de snelheid met 2
 					motorspeed = 2;
 				}
-				if (motorspeed == 0 && liftvraag0 == false){
+				if (motorspeed == 0 && liftvraag0 == false){//als de lift op verdieping 0 is en er is geen liftvraag op verdieping 0 verander de motor richting naar boven
 					motorRichting = true;
 				}
 				
 			}
 		}
-		if (huidigeLocatie == 6){
-			if (motorRichting == true){
+		if (huidigeLocatie == 6){//als de locatie 6 is voer het volgende uit
+			if (motorRichting == true){//als de lift naar boven gaat en de snelheid is 8 verlaag deze naar 6
 				if(motorspeed == 8){
 					motorspeed = 6;
 				}
-				else if (motorspeed != 6){
+				else if (motorspeed != 6){//als de snelheid niet 6 is verlaag de snelheid naar 2
 					motorspeed = 2;
 				}
 			}
-			if(motorRichting == false){
-				if(liftvraag0 == true && liftvraag1 == false){
+			if(motorRichting == false){//als de motor naar beneden staat voer het volgende uit
+				if(liftvraag0 == true && liftvraag1 == false){//als de lift naar verdieping 0 moet en niet naar 1 verander de snelheid naar 6
 					motorspeed = 6;
 				}
-				else if (liftvraag1 == true){
+				else if (liftvraag1 == true){//als de lift naar verdieping 1 moet verlaag de snelheid naar 0
 					motorspeed = 2;
 				}
 			}
 		}
-		if (huidigeLocatie == 7){
+		if (huidigeLocatie == 7){//als de lift op positie 7 is verlaag de snelheid naar 4
 			motorspeed = 4;
 		}
-		if (huidigeLocatie == 8){
+		if (huidigeLocatie == 8){//als de locatie 8 is verlaag de snelheid naar 2
 			motorspeed = 2;
 		}
-		if (huidigeLocatie == 9){
-			if (liftvraag2 == true){
-				motorspeed = 0;
-				liftvraag2 = false;
+		if (huidigeLocatie == 9){//als de lift op locatie 9 is voer het volgende uit
+			if (liftvraag2 == true){//als de lift naar 2 moet voer het volgende uit
+				motorspeed = 0;//zet de motor uit
+				liftvraag2 = false;//schakel die lift aanvraag voor verdieping 2 uit
 				lift.setMotorSpeed(motorspeed);
-				lift.setDoorOpen();
+				lift.setDoorOpen();//zet de lift deuren open en sluit ze weer na 5 seconden
 				sleep(5000);
 				lift.setDoorClose();
 				sleep(5000);
-				motorRichting = false;
+				motorRichting = false;//zet de liftrichting naar beneden
 			}
+			//als de motorrichting naar beneden staat en de lift naar verdieping 1 of 0 moet zet de snelheid op 2
 			if (motorRichting == false && (liftvraag1 == true || liftvraag0 == true))
 			{
 				motorspeed = 2;
